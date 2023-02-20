e22049c35804d0cfd59ce7802aa72a2bd7522575
Joakim Holmberg <joakimho@student.chalmers.se>
2023-02-20 20:36:25 +0100

    Revert "Temporary test code for testing GameService"
    This reverts commit 381dab11220da25c5339ff584b59a3995649fbff.

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,13 @@
 import LoginPage from './Components/Login/LoginPage';
 import HomePage from './Components/Home/HomePage';
-import GameService from './Components/Services/GameService'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from 'react';
-
-let gameService: GameService = new GameService();
 
 function App() {
-
-  const [playerId, setPlayerId] = useState<string>("...");
-  const [gameId, setGameId] = useState<number>(0)
-
-  function inc() {
-    setGameId(gameId + 1);
-  }
-
-  function joinGame()
-  {
-    gameService.joinGame(`${gameId}`);
-  }
-
-  gameService.hubConnection.on('playerJoinedGame', (value) => {
-      setPlayerId(value);
-  });
-  
-
   return (
-    <>
-      <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-      <button onClick={joinGame}>Join Game</button>
-      <button onClick={inc}>{gameId}</button>
-      <span> {playerId} </span>
-    </>
+    <Routes>
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
