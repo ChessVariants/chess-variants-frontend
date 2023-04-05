cb42d31d96e1794bcd331257a34cf0bdcedef6a4
ValleSten <valdemar.stenhammar@hotmail.com>
2023-04-05 17:16:51 +0200

    Add function to create a new pattern

--- src/Components/Editor/EditorSidePage.tsx
+++ src/Components/Editor/EditorSidePage.tsx
@@ -1,9 +1,9 @@
-import { Box, Button, InputLabel, MenuItem, Select, SelectChangeEvent, TextField } from "@mui/material";
-import { FormControl, Theme } from "@material-ui/core";
+import { Accordion, AccordionSummary, Box, Button, InputLabel, MenuItem, Select, SelectChangeEvent, Stack, TextField, Typography } from "@mui/material";
+import { AccordionDetails, FormControl, Theme } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 import CustomButton from "../Util/CustomButton";
 import EditorService from "../../Services/EditorService";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 
 const useStyles = makeStyles<Theme>(theme => ({
@@ -34,98 +34,112 @@ export default function SideInfo(props: { editorService: EditorService }) {
     const classes = useStyles();
 
     const [xDir, setXDir] = useState('');
-    const handleXDirChange = (event: SelectChangeEvent) => {
-        setXDir(event.target.value);
+    const handleXDirChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        const newValue = event.target.value;
+        if (newValue === "" || newValue === "-" || /^-?([01])$/.test(newValue)) {
+            setXDir(newValue);
+        }
     };
 
     const [yDir, setYDir] = useState('');
-    const handleYDirChange = (event: SelectChangeEvent) => {
-        setYDir(event.target.value);
+    const handleYDirChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        const newValue = event.target.value;
+        if (newValue === "" || newValue === "-" || /^-?([01])$/.test(newValue)) {
+            setYDir(newValue);
+        }
     };
-    
+
     const [minLength, setMinLength] = useState('');
-    const handleMinLengthChange = (event: SelectChangeEvent) => {
-        setMinLength(event.target.value);
+    const handleMinLengthChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        const newValue = event.target.value;
+        if (newValue === "" || /^(1?[1-9]|20)$/.test(newValue) && parseInt(newValue) <= 20) {
+            setMinLength(newValue);
+        }
     };
 
     const [maxLength, setMaxLength] = useState('');
-    const handleMaxLengthChange = (event: SelectChangeEvent) => {
-        setMaxLength(event.target.value);
+    const handleMaxLengthChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        const newValue = event.target.value;
+        if (newValue === "" || /^(1?[1-9]|20)$/.test(newValue) && parseInt(newValue) <= 20) {
+            setMaxLength(newValue);
+        }
+    };
+
+    const [xOffset, setXOffset] = useState('');
+    const handleXOffsetChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        const newValue = event.target.value;
+        if (newValue === "" || newValue === "-" || /^-?(0|[1-9][0-9]?)?$/.test(newValue) && parseInt(newValue) <= 20) {
+            setXOffset(newValue);
+        }
+    };
+
+    const [yOffset, setYOffset] = useState('');
+    const handleYOffsetChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        const newValue = event.target.value;
+        if (newValue === "" || newValue === "-" || /^-?(0|[1-9][0-9]?)?$/.test(newValue) && parseInt(newValue) <= 20) {
+            setYOffset(newValue);
+        }
     };
 
     return (
         <Box className={classes.Container}>
-            <p>Settings</p>
-            <CustomButton text={"Leave"} color={"blue"} height={"32px"}></CustomButton>
-            <Button onClick={() => { editorService.addMovementPattern(Number(xDir), Number(yDir), Number(minLength), Number(maxLength)) }}>Add Pattern</Button>
-            <Button onClick={() => { editorService.requestBoardState() }}>Request state</Button>
-            <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">X-direction</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={xDir}
-                    label="xDir"
-                    onChange={handleXDirChange}
+            <h2>Create a new Piece</h2>
+            <Accordion>
+                <AccordionSummary
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
                 >
-                    <MenuItem value={-1}>-1</MenuItem>
-                    <MenuItem value={0}>0</MenuItem>
-                    <MenuItem value={1}>1</MenuItem>
-                </Select>
-            </FormControl>
-            <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Y-direction</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={yDir}
-                    label="xDir"
-                    onChange={handleYDirChange}
+                    <Typography>Regular Pattern</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <FormControl fullWidth>
+                        <Stack spacing={2} alignItems="center">
+                            <div>
+                                <label htmlFor="xDir" style={{ display: "inline-block" }}>X direction:</label>
+                                <input id="xDir" type="text" value={xDir} onChange={handleXDirChange} style={{ width: "20px", position: "relative", marginLeft: "10px" }} />
+                            </div>
+                            <div>
+                                <label htmlFor="yDir" style={{ display: "inline-block" }}>Y direction:</label>
+                                <input id="yDir" type="text" value={yDir} onChange={handleYDirChange} style={{ width: "20px", position: "relative", marginLeft: "10px" }} />
+                            </div>
+                            <div>
+                                <label htmlFor="minLength" style={{ display: "inline-block" }}>Minimum length:</label>
+                                <input id="minLength" type="text" value={minLength} onChange={handleMinLengthChange} style={{ width: "20px", position: "relative", marginLeft: "10px" }} />
+                            </div>
+                            <div>
+                                <label htmlFor="maxLength" style={{ display: "inline-block" }}>Maximum length:</label>
+                                <input id="maxLength" type="text" value={maxLength} onChange={handleMaxLengthChange} style={{ width: "20px", position: "relative", marginLeft: "10px" }} />
+                            </div>
+                            <Button onClick={() => { editorService.addMovementPattern(Number(xDir), Number(yDir), Number(minLength), Number(maxLength)) }}>Add Pattern</Button>
+                        </Stack>
+                    </FormControl>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    aria-controls="panel2a-content"
+                    id="panel2a-header"
                 >
-                    <MenuItem value={-1}>-1</MenuItem>
-                    <MenuItem value={0}>0</MenuItem>
-                    <MenuItem value={1}>1</MenuItem>
-                </Select>
-            </FormControl>
-            <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Minimum length</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={minLength}
-                    label="minLength"
-                    onChange={handleMinLengthChange}
-                >
-                    <MenuItem value={1}>1</MenuItem>
-                    <MenuItem value={2}>2</MenuItem>
-                    <MenuItem value={3}>3</MenuItem>
-                    <MenuItem value={4}>4</MenuItem>
-                    <MenuItem value={5}>5</MenuItem>
-                    <MenuItem value={6}>6</MenuItem>
-                    <MenuItem value={7}>7</MenuItem>
-                    <MenuItem value={8}>8</MenuItem>
+                    <Typography>Jump pattern</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <FormControl fullWidth>
+                        <Stack spacing={2} alignItems="center">
+                            <div>
+                                <label htmlFor="xOffset" style={{ display: "inline-block" }}>X Offset:</label>
+                                <input id="xOffset" type="text" value={xOffset} onChange={handleXOffsetChange} style={{ width: "20px", position: "relative", marginLeft: "10px" }} />
+                            </div>
+                            <div>
+                                <label htmlFor="yOffset" style={{ display: "inline-block" }}>Y Offset:</label>
+                                <input id="yOffset" type="text" value={yOffset} onChange={handleYOffsetChange} style={{ width: "20px", position: "relative", marginLeft: "10px" }} />
+                            </div>
+                            <Button onClick={() => { editorService.addMovementPattern(Number(xOffset), Number(yOffset), -1, -1) }}>Add Pattern</Button>
+                        </Stack>
+                    </FormControl>
 
-                </Select>
-            </FormControl>
-            <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Maximum length</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={maxLength}
-                    label="maxLength"
-                    onChange={handleMaxLengthChange}
-                >
-                    <MenuItem value={1}>1</MenuItem>
-                    <MenuItem value={2}>2</MenuItem>
-                    <MenuItem value={3}>3</MenuItem>
-                    <MenuItem value={4}>4</MenuItem>
-                    <MenuItem value={5}>5</MenuItem>
-                    <MenuItem value={6}>6</MenuItem>
-                    <MenuItem value={7}>7</MenuItem>
-                    <MenuItem value={8}>8</MenuItem>
-                </Select>
-            </FormControl>
+                </AccordionDetails>
+            </Accordion>
+
         </Box>
     );
 
