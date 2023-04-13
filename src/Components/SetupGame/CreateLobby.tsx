84bf3d4eff5123c429e2faf747e6393b92899c65
bobby <gp.bobbelty@gmail.com>
2023-04-13 11:33:44 +0200

    Preparation for TryCreateLobby

--- src/Components/SetupGame/CreateLobby.tsx
+++ src/Components/SetupGame/CreateLobby.tsx
@@ -1,9 +1,10 @@
-import { Button, Container, Divider, Grid, IconButton, MenuItem, Paper, Select, SelectChangeEvent, styled, TextField, Typography } from "@mui/material";
+import { Box, Button, Container, Divider, Grid, IconButton, MenuItem, Paper, Select, SelectChangeEvent, styled, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import { commonClasses } from "../Util/CommonClasses";
 import CustomDarkTheme from "../Util/CustomDarkTheme";
 import useMediaQuery from '@mui/material/useMediaQuery';
 import ClearIcon from "@mui/icons-material/Clear";
+import GameService from "../../Services/GameService";
 
 
 
@@ -12,21 +13,34 @@ export default function CreateGame(props: { createGameFunction: any }) {
     const classes = commonClasses();
     const [selectVariant, setSelectVariant] = useState('standard');
     const [textFieldVariant, setTextFieldVariant] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const gameService = GameService.getInstance();
 
     const clearTextField = () => {
         setTextFieldVariant("");
         setSelectVariant("standard");
     };
 
-    const createLobby = () => {
-        if (textFieldVariant === "") {
-            props.createGameFunction(selectVariant);
+    // Placeholder
+    const tryCreateLobby = () => {
+        if ("success") {
+            if (textFieldVariant === "") {
+                createLobby(selectVariant);
+            }
+            else {
+                createLobby(textFieldVariant);
+            }
         }
         else {
-            props.createGameFunction(textFieldVariant);
+            setErrorMessage("Please enter a valid variant code!");
         }
-
     };
+    const createLobby = (variantId: string) => {
+        const gameID = (Math.random() + 1).toString(36).substring(5);
+        gameService.sendCreateGame(gameID, variantId);
+        props.createGameFunction(variantId);
+    }
 
     const selectSetVariant = (event: SelectChangeEvent) => {
         setSelectVariant(event.target.value as string);
@@ -110,11 +124,19 @@ export default function CreateGame(props: { createGameFunction: any }) {
                     color={"createColor"}
                     fullWidth
                     variant="contained"
-                    onClick={createLobby}
+                    onClick={tryCreateLobby}
                     sx={{ mt: 3, mb: 1, p: 1 }}
                 >
                     CREATE LOBBY
                 </Button>
+                {
+                    errorMessage ? <Box sx=
+                        {{
+                            color: CustomDarkTheme.palette.error.main,
+                            fontSize: "12px",
+                        }}>{errorMessage}</Box>
+                        : null
+                }
             </Paper>
         </Container>
     )
--- src/Components/SetupGame/SetupGame.tsx
+++ src/Components/SetupGame/SetupGame.tsx
@@ -14,11 +14,9 @@ export default function NewGame(props: { pageState?: pageStates }) {
     const [pageState, setPageState] = useState(props.pageState ? props.pageState : pageStates.CreateGame);
     const [gameID, setGameID] = useState("initial_gameid");
 
-    const createGameFunction = (variantID: string) => {
-        const gameID = (Math.random() + 1).toString(36).substring(5);
+    const createGameFunction = (gameID: string) => {
         setGameID(gameID);
         setPageState(pageStates.Lobby);
-        gameService.sendCreateGame(gameID, variantID);
     }
 
     if (pageState === pageStates.CreateGame) {
