d39698afeed27362fc6c62eb6269962cc71a2a85
bobby <gp.bobbelty@gmail.com>
2023-04-14 20:30:59 +0200

    Added custom pieces svgs
    Also added imports to those images in PieceImageAdapter but those are not used

--- src/IMG/PieceImageAdapter.tsx
+++ src/IMG/PieceImageAdapter.tsx
@@ -5,6 +5,29 @@ import knight from './knight.svg';
 import queen from './queen.svg';
 import king from './king.svg';
 
+import cannon from './cannon.svg'
+import commoner from './commoner.svg'
+import flag from './flag.svg'
+import giraffe from './giraffe.svg'
+import halfmoon from './halfmoon.svg'
+import joker from './joker.svg'
+import knightrotated from './knightrotated.svg'
+import knightroyal from './knightroyal.svg'
+import knighttie from './knighttie.svg'
+import knightunicorn from './knightunicorn.svg'
+import knightvar1 from './knightvar1.svg'
+import knightvar2 from './knightvar2.svg'
+import knightzebra from './knightzebra.svg'
+import pawnvar1 from './pawnvar1.svg'
+import pawnvar2 from './pawnvar2.svg'
+import pawnvar3 from './pawnvar3.svg'
+import plus from './plus.svg'
+import ship from './ship.svg'
+import supertower from './supertower.svg'
+import telescope from './telescope.svg'
+import tower from './tower.svg'
+
+
 /**
  * Adapter a with static method that matches the id of a piece with the correct image
  */
