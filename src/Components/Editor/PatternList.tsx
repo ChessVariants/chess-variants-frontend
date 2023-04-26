b1946021e3c64f66303d0ad1cba4b2238f1bea3a
ValleSten <valdemar.stenhammar@hotmail.com>
2023-04-26 15:34:36 +0200

    Add logic to pattern list buttons

--- src/Components/Editor/PatternList.tsx
+++ src/Components/Editor/PatternList.tsx
@@ -8,14 +8,9 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import EditorService, { PatternState } from "../../Services/EditorService";
-import { Button, Container, IconButton, Theme } from "@mui/material";
-import { FormControl, FormGroup, Typography } from "@material-ui/core";
+import { Button, Container, Theme } from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
-import ExpandLessIcon from '@mui/icons-material/ExpandLess';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { maxHeaderSize } from "http";
-import darkScrollbar from '@mui/material/darkScrollbar';
-
+import PatternListCell from "./PatternListCell";
 
 const useStyles = makeStyles<Theme>(theme => ({
     Container: {
@@ -26,16 +21,16 @@ const useStyles = makeStyles<Theme>(theme => ({
     },
     scrollbar: {
         '&::-webkit-scrollbar': {
-          width: '0.4em',
+            width: '0.4em',
         },
         '&::-webkit-scrollbar-track': {
-          backgroundColor: '#000000',
+            backgroundColor: '#000000',
         },
         '&::-webkit-scrollbar-thumb': {
-          backgroundColor: '#888',
-          borderRadius: '20px',
+            backgroundColor: '#888',
+            borderRadius: '20px',
         },
-      },
+    },
 
 }));
 
@@ -54,17 +49,16 @@ export default function PatternList(props: { editorID: string }) {
     const { editorID } = props;
 
     useEffect(() => {
-
         editorService.on("PatternAdded", (patternState: PatternState) => {
             setPatternState(patternState);
         })
-    }, [])
+    })
 
     return (
         <Container>
             <Paper className={classes.Container}>
                 <h3>Movement patterns</h3>
-                <TableContainer className={classes.scrollbar} component={Paper} style={{ overflow: "auto", height: 700}}>
+                <TableContainer className={classes.scrollbar} component={Paper} style={{ overflow: "auto", height: 700 }}>
                     <Table size="small" style={{
                         width: "100%",
                         flexDirection: "column",
@@ -84,66 +78,11 @@ export default function PatternList(props: { editorID: string }) {
                                 <TableRow
                                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                 >
-                                    <TableCell align="center">
-                                        <IconButton size="small" onClick={() => {
-                                            editorService.removeMovementPattern(editorID, pattern.xDir, pattern.yDir, pattern.minLength, pattern.maxLength)
-                                            editorService.addMovementPattern(editorID, pattern.xDir, pattern.yDir + 1, pattern.minLength, pattern.maxLength)
-                                        }}>
-                                            <ExpandLessIcon fontSize="inherit" />
-                                        </IconButton>
-                                        <Typography> {pattern.yDir} </Typography>
-                                        <IconButton size="small" onClick={() => {
-                                            editorService.removeMovementPattern(editorID, pattern.xDir, pattern.yDir, pattern.minLength, pattern.maxLength)
-                                            editorService.addMovementPattern(editorID, pattern.xDir, pattern.yDir - 1, pattern.minLength, pattern.maxLength)
-                                        }}>
-                                            <ExpandMoreIcon fontSize="inherit" />
-                                        </IconButton>
-                                    </TableCell>
-                                    <TableCell align="center">
-                                        <IconButton size="small" onClick={() => {
-                                            editorService.removeMovementPattern(editorID, pattern.xDir, pattern.yDir, pattern.minLength, pattern.maxLength)
-                                            editorService.addMovementPattern(editorID, pattern.xDir + 1, pattern.yDir, pattern.minLength, pattern.maxLength)
-                                        }}>
-                                            <ExpandLessIcon fontSize="inherit" />
-                                        </IconButton>
-                                        <Typography> {pattern.xDir} </Typography>
-                                        <IconButton size="small" onClick={() => {
-                                            editorService.removeMovementPattern(editorID, pattern.xDir, pattern.yDir, pattern.minLength, pattern.maxLength)
-                                            editorService.addMovementPattern(editorID, pattern.xDir - 1, pattern.yDir, pattern.minLength, pattern.maxLength)
-                                        }}>
-                                            <ExpandMoreIcon fontSize="inherit" />
-                                        </IconButton>
-                                    </TableCell>
-                                    <TableCell align="center">
-                                        <IconButton size="small" onClick={() => {
-                                            editorService.removeMovementPattern(editorID, pattern.xDir, pattern.yDir, pattern.minLength, pattern.maxLength)
-                                            editorService.addMovementPattern(editorID, pattern.xDir, pattern.yDir, pattern.minLength + 1, pattern.maxLength)
-                                        }}>
-                                            <ExpandLessIcon fontSize="inherit" />
-                                        </IconButton>
-                                        <Typography> {pattern.minLength} </Typography>
-                                        <IconButton size="small" onClick={() => {
-                                            editorService.removeMovementPattern(editorID, pattern.xDir, pattern.yDir, pattern.minLength, pattern.maxLength)
-                                            editorService.addMovementPattern(editorID, pattern.xDir, pattern.yDir, pattern.minLength - 1, pattern.maxLength)
-                                        }}>
-                                            <ExpandMoreIcon fontSize="inherit" />
-                                        </IconButton>
-                                    </TableCell>
-                                    <TableCell align="center">
-                                        <IconButton size="small" onClick={() => {
-                                            editorService.removeMovementPattern(editorID, pattern.xDir, pattern.yDir, pattern.minLength, pattern.maxLength)
-                                            editorService.addMovementPattern(editorID, pattern.xDir, pattern.yDir, pattern.minLength, pattern.maxLength + 1)
-                                        }}>
-                                            <ExpandLessIcon fontSize="inherit" />
-                                        </IconButton>
-                                        <Typography> {pattern.maxLength} </Typography>
-                                        <IconButton size="small" onClick={() => {
-                                            editorService.removeMovementPattern(editorID, pattern.xDir, pattern.yDir, pattern.minLength, pattern.maxLength)
-                                            editorService.addMovementPattern(editorID, pattern.xDir, pattern.yDir, pattern.minLength, pattern.maxLength - 1)
-                                        }}>
-                                            <ExpandMoreIcon fontSize="inherit" />
-                                        </IconButton>
-                                    </TableCell>
+                                    <PatternListCell editorID={editorID} xDir={pattern.xDir} yDir={pattern.yDir} minLength={pattern.minLength} maxLength={pattern.maxLength} attr={"xDir"} />
+                                    <PatternListCell editorID={editorID} xDir={pattern.xDir} yDir={pattern.yDir} minLength={pattern.minLength} maxLength={pattern.maxLength} attr={"yDir"} />
+                                    <PatternListCell editorID={editorID} xDir={pattern.xDir} yDir={pattern.yDir} minLength={pattern.minLength} maxLength={pattern.maxLength} attr={"minLength"} />
+                                    <PatternListCell editorID={editorID} xDir={pattern.xDir} yDir={pattern.yDir} minLength={pattern.minLength} maxLength={pattern.maxLength} attr={"maxLength"} />
+
                                     <TableCell align="center">{<Button onClick={() => {
                                         editorService.removeMovementPattern(editorID, pattern.xDir, pattern.yDir, pattern.minLength, pattern.maxLength)
                                     }}
